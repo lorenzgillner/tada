1d26ac113f0b1e09c28b64bf07e1942b6b721aa2
gillor <lorenz.gillner@hs-wismar.de>
2024-10-16 16:55:05 +0200

    Add unit tests

---
+++ test/arithmetic.cpp
@@ -0,0 +1,52 @@
+#define BOOST_TEST_MODULE arithmetic
+#include "boost/test/included/unit_test.hpp"
+#include "../tada.hpp"
+
+using namespace tada;
+
+typedef double itype;
+typedef Derivable<itype> dtype;
+
+BOOST_AUTO_TEST_CASE( test_add )
+{
+    auto f = [](dtype x) { return x + 2; };
+
+    dtype x(2);
+    dtype fx = f(x.derive());
+
+    BOOST_TEST(fx.v() == 4.0);
+    BOOST_TEST(fx.d() == 1.0);
+}
+
+BOOST_AUTO_TEST_CASE( test_sub )
+{
+    auto f = [](dtype x) { return x - 2; };
+
+    dtype x(2);
+    dtype fx = f(x.derive());
+
+    BOOST_TEST(fx.v() == 0.0);
+    BOOST_TEST(fx.d() == 1.0);
+}
+
+BOOST_AUTO_TEST_CASE( test_mul )
+{
+    auto f = [](dtype x) { return x * 2; };
+
+    dtype x(2);
+    dtype fx = f(x.derive());
+
+    BOOST_TEST(fx.v() == 4.0);
+    BOOST_TEST(fx.d() == 2.0);
+}
+
+BOOST_AUTO_TEST_CASE( test_div )
+{
+    auto f = [](dtype x) { return x / 2; };
+
+    dtype x(2);
+    dtype fx = f(x.derive());
+
+    BOOST_TEST(fx.v() == 1.0);
+    BOOST_TEST(fx.d() == 0.5);
+}
