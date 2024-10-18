c2394224a178dbcbc741fe7e272d7d61240891aa
gillor <lorenz.gillner@hs-wismar.de>
2024-10-18 18:06:14 +0200

    use intervals for testing

--- tests/config.hpp
+++ tests/config.hpp
@@ -1,5 +1,15 @@
 #include "../tada.hpp"
 #include <boost/mpl/list.hpp>
+#include <boost/numeric/interval.hpp>
+#include <boost/numeric/interval/io.hpp>
 #include <boost/test/included/unit_test.hpp>
 
-typedef boost::mpl::list<float, double> test_types;
+using namespace boost;
+using namespace numeric;
+using namespace interval_lib;
+
+typedef interval<double, policies<save_state<rounded_transc_std<double>>,
+                                  checking_base<double>>>
+    custom_interval;
+
+typedef boost::mpl::list<float, double, interval<float>, interval<double>, custom_interval> test_types;
